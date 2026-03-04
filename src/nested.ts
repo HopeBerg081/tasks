ce03573404a2144be43ae96e105636192f146e2e
HopeBerg081 <hopeberg18@gmail.com>
2026-03-04 16:49:51 -0500

    Task 7) Finished getAnswers function

--- src/nested.ts
+++ src/nested.ts
@@ -94,7 +94,19 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+	interface Answers {
+		questionId: number,
+		text: string,
+		submitted: boolean
+		correct: boolean
+	}
+	const answers: Answers[] = questions.map(question => ({
+		questionId: question.id,
+		text: "",
+		submitted: false,
+		correct: false
+	}));
+	return answers;
 }
 
 /***
