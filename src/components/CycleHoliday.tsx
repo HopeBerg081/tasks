ad285b2429ca8a2dc12c558b3a2049af86d04ecc
HopeBerg081 <hopeberg18@gmail.com>
2026-03-07 12:46:41 -0500

    Task 8) Prepared other files so I can test CycleHoliday. Set up Holiday type, buttons, and Records

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,7 +1,32 @@
-// import React, { useState } from "react";
-// import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 
-// export function CycleHoliday(): React.JSX.Element {
-//     return <div>Cycle Holiday</div>;
-// }
-export {};
+export function CycleHoliday(): React.JSX.Element {
+    type Holiday =
+        | "Halloween"
+        | "Christmas"
+        | "Rosh Hashanah"
+        | "Valentine's Day"
+        | "Christmas Eve";
+    const [holiday, setHoliday] = useState<Holiday>("Halloween");
+    const SORT_ALPHABET: Record<Holiday, Holiday> = {
+        Christmas: "Christmas Eve",
+        "Christmas Eve": "Halloween",
+        Halloween: "Rosh Hashanah",
+        "Rosh Hashana": "Valentine's Day",
+        "Valentine's Day": "Christmas",
+    };
+    const SORT_TIME: Record<Holiday, Holiday> = {
+        "Valentine's Day": "Rosh Hashanah",
+        "Rosh Hashana": "Halloween",
+        Halloween: "Christmas Eve",
+        "Christmas Eve": "Christmas",
+        Christmas: "Valentine's Day",
+    };
+    return (
+        <div>
+            <Button>Cycle by Alphabetical Order</Button>
+            <Button>Cycle by Time in Year</Button>
+        </div>
+    );
+}
