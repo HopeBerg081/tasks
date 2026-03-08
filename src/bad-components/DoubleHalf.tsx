fa6f920f781e513609ed92b607d22c4e28d1aeb4
HopeBerg081 <hopeberg18@gmail.com>
2026-03-08 12:38:59 -0400

    Task 9) Changed function in DoubleHalf to be in the broader DoubleHalf function, as well as seperating the button parts of their logic from their algebriac parts. Now trying to figure out problem with parsing and the useState

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -2,39 +2,24 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(2 * dhValue);
-            }}
-        >
-            Double
-        </Button>
-    );
-}
-
-function Halver(): React.JSX.Element {
-    return (
-        <Button
-            onClick={() => {
-                setDhValue(0.5 * dhValue);
-            }}
-        >
-            Halve
-        </Button>
-    );
-}
-
 export function DoubleHalf(): React.JSX.Element {
+    const [dhValue, setDhValue] = useState<number>(1); //Need to define state before using it
+    
+    function Doubler(): void { //Function needs to be within the larger function
+        setDhValue((prev) => prev * 2); //Using prev can prevent errors
+    }
+
+    function Halver(): void {
+        setDhValue((prev) => prev * 0.5)
+    }
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Doubler></Doubler>
-            <Halver></Halver>
+            <Button onClick = {Doubler}>Double</Button>
+            <Button onClick = {Halver()}>Halver</Button>
         </div>
     );
 }
