b5dd034ac4d47603ad2e9eed09f279422a0a3314
HopeBerg081 <hopeberg18@gmail.com>
2026-03-08 14:11:57 -0400

    Task 9) Realized that by removing the React.JSX.Element return type from the Doubler and Halver functions, they were no longer components, whcih went against the assignment's instructions. Have started trying to work with these constraints instead

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+interface DhProps {
+    setDhValue: (newDhNumber : number) => number;
+}
+
+function Doubler({setDhValue}: DhProps): React.JSX.Element {
+    return (
+        <Button onClick= {() => setDhValue((prev) => prev * 2)}>
+        Double</Button>
+    );
+}
+
+function Halver(): React.JSX.Element {
+    return (
+        <Button onClick {() => setDhValue((prev)=> prev * 0.5)}>
+        Halve</Button>
+    );
+}
+
 export function DoubleHalf(): React.JSX.Element {
     const [dhValue, setDhValue] = useState<number>(10);
-
-    function Doubler(): void { //Function needs to be within the larger function
-        setDhValue((prev) => prev * 2); //Using prev can prevent errors
-    }
-
-    function Halver(): void {
-        setDhValue((prev) => prev * 0.5)
-    }
     return (
         <div>
             <h3>Double Half</h3>
             <div>
                 The current value is: <span>{dhValue}</span>
             </div>
-            <Button onClick = {Doubler}>Double</Button>
-            <Button onClick = {Halver}>Halver</Button>
+            <Doubler setDhCalue = {setDhValue} />
+            <Halver setDhValue = {setDhValue} />
         </div>
     );
 }
