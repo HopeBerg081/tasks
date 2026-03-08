1412dfc1aab2e25c3ca48c3dafd6792911f435af
HopeBerg081 <hopeberg18@gmail.com>
2026-03-08 16:36:53 -0400

    Task 9) Working on ShoveBox.tsx. Tried to add an interface so that the position state can be used between multiple functions

--- src/bad-components/ShoveBox.tsx
+++ src/bad-components/ShoveBox.tsx
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-function ShoveBoxButton({
-    position,
-    setPosition,
-}: {
-    position: number;
+interface positionProps {
+    position : number;
     setPosition: (newPosition: number) => void;
-}) {
-    return (
+}
+
+function ShoveBoxButton({position, setPosition} : positionProps){
+     return (
         <Button
             onClick={() => {
                 setPosition(4 + position);
             }}
-        >
-            Shove the Box
-        </Button>
+        >Shove the Box</Button>
     );
 }
 
@@ -31,26 +28,25 @@ function MoveableBox(): React.JSX.Element {
                 border: "1px solid blue",
                 display: "inline-block",
                 verticalAlign: "bottom",
-                marginLeft: position + "px",
+                marginLeft: {position} + "px",
             }}
         ></div>
     );
 }
 
-export function ShoveBox(): React.JSX.Element {
+export function ShoveBox({position, setPosition} : positionProps): React.JSX.Element {
     const box = MoveableBox();
-
     return (
         <div>
             <h3>Shove Box</h3>
-            {/* <span>The box is at: {box.position}</span>
+            <span>The box is at: {box.position}</span>
             <div>
                 <ShoveBoxButton
                     position={box.position}
                     setPosition={box.setPosition}
                 ></ShoveBoxButton>
                 {box}
-            </div> */}
+            </div>
         </div>
     );
 }
