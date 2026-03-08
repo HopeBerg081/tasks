cb08977fae9464da9ac715574f76c33c04d4ffe7
HopeBerg081 <hopeberg18@gmail.com>
2026-03-08 14:58:31 -0400

    Task 9)Fixed setAllOptions in clearTeam by changing how its implemented in the Button

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -17,13 +17,13 @@ export function ChooseTeam(): React.JSX.Element {
     function chooseMember(newMember: string) {
         if (!team.includes(newMember)) {
             setTeam(prevTeam => [...prevTeam, newMember]);
-            setAllOptions(prevAllOptions => prevAllOptions.filter(PEOPLE => PEOPLE === newMember));
+            setAllOptions(prevAllOptions => prevAllOptions.filter(PEOPLE => PEOPLE !== newMember));
         }
     }
 
     function clearTeam() {
         setTeam([]);
-        setAllOptions([]);
+        setAllOptions(PEOPLE);
     }
 
     return (
@@ -45,7 +45,7 @@ export function ChooseTeam(): React.JSX.Element {
                     {team.map((member: string) => (
                         <li key={member}>{member}</li>
                     ))}
-                    <Button onClick={() => {clearTeam}}>Clear Team</Button>
+                    <Button onClick={() => {clearTeam()}}>Clear Team</Button>
                 </Col>
             </Row>
         </div>
