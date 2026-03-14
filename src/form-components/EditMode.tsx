d5d81ee160c724ad989f80c64fd42ff8d885b612
HopeBerg081 <hopeberg18@gmail.com>
2026-03-14 19:19:09 -0400

    Task 10) EditMode seems to be working correctly in the app, though it is still running into trouble with testing

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,52 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [userName, setUserName] = useState<string>("Your Name");
+    const [isStudent, setIsStudent] = useState<boolean>(true);
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(event.target.checked)
+    }
+    function updateUserName (event: React.ChangeEvent<HTMLInputElement>) {
+        setUserName(event.target.value);
+    }
+    function updateIsStudent (event: React.ChangeEvent<HTMLInputElement>) {
+        setIsStudent(event.target.checked);
+    }
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Group controlId = "formChooseEditMode">
+                <Form.Label>Edit Mode</Form.Label>
+                <Form.Check
+                    type = "switch"
+                    id = "edit-mode-check"
+                    checked = {editMode}
+                    onChange = {updateEditMode}
+                />
+            </Form.Group>
+            {editMode ?  (
+                <div>
+                    <Form.Group controlId = "formEditUserName">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            type = "userName"
+                            value = {userName}
+                            onChange = {updateUserName}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId = "formEditIsStudent">
+                        <Form.Label>Are you a student? </Form.Label>
+                        <Form.Check
+                            type = "switch"
+                            id = "is-student-check"
+                            checked = {isStudent}
+                            onChange = {updateIsStudent}
+                        />
+                    </Form.Group>
+                </div>
+            ) : undefined}
+            <div>{userName} is {isStudent ? "" : "not "}a student</div>
         </div>
     );
 }
