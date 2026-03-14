76ea4e9729668ce8371c2ea557c313c4511dca44
HopeBerg081 <hopeberg18@gmail.com>
2026-03-14 16:54:53 -0400

    Task 10) Figured out the basics of using form in CheckAnswer. However, need to compare givenAnswer to expectedAnswer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
+import Form from "react-bootstrap/Form"
 
 export function CheckAnswer({
     expectedAnswer,
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const[givenAnswer, setGivenAnswer] = useState<string>("");
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setGivenAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formCheckAnswer"> 
+                <Form.Label>Check Answer</Form.Label>
+                <Form.Control value={givenAnswer} onChange={updateAnswer}/>
+            </Form.Group>
+            <div>
+
+            </div>
         </div>
     );
 }
