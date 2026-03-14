8a850dc9a59c1523a6edfc81dce52b46673b3e79
HopeBerg081 <hopeberg18@gmail.com>
2026-03-14 17:21:52 -0400

    Task 10) Started working on GiveAttempts. Having some problems with type and disabling the button

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 export function GiveAttempts(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    function updateAttempts(event: React.ChangeEvent<HTMLInputElement>) {
+        if (Number.isNaN(event)) {
+            setAttempts(parseInt(event.target.value));
+        } else {
+            return;
+        }
+    }
     return (
         <div>
-            <h3>Give Attempts</h3>
+            <h2>Attempts left = {attempts}</h2>
+            <Form.Group controlId="formGiveAttempts"> 
+                <Form.Label>Give Attempts</Form.Label>
+                <Form.Control>
+                    type = "number"
+                    value={attempts}
+                    onChange={updateAttempts}
+                </Form.Control>
+            </Form.Group>
+            <Button {{attempts} <= 0 ? disabled : !disabled}
+                onClick ={()=> {setAttempts(attempts - 1)}}>use</Button>
+            <Button onClick ={()=> {setAttempts(attempts + 1)}}>gain</Button>
         </div>
     );
 }
