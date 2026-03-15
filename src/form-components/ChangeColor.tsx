c104694b75223c4fb89a2fb7737eaf09b57d005a
HopeBerg081 <hopeberg18@gmail.com>
2026-03-14 20:31:51 -0400

    Task 10) Have two buttons working for ChangeChange. Want to figure out how to use map in this example to simplify the code

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,35 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red");
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Check
+                inline
+                type = "radio"
+                name = "color"
+                onChange = {updateColor}
+                id = "color-check-red"
+                label = "Red"
+                value = "red"
+                checked = {color === "red"}
+            />
+            <Form.Check
+                inline
+                type = "radio"
+                name = "color"
+                onChange = {updateColor}
+                id = "color-check-orange"
+                label = "Orange"
+                value = "orange"
+                checked = {color === "orange"}
+            />
+            <div>You have chosen <span style={{backgroundColor: color}}>{color}</span>.</div>
         </div>
     );
 }
