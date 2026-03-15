c806345bb27072c8a1526ffd242008f468500e42
HopeBerg081 <hopeberg18@gmail.com>
2026-03-14 20:52:20 -0400

    Task 10) Got ChangeColor to work except it doesn't have the switches colored as well

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const colors = ["red", "orange", "yellow", "green", "cyan", "blue", "purple", "pink"];
     const [color, setColor] = useState<string>("red");
     function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
         setColor(event.target.value);
@@ -9,26 +10,19 @@ export function ChangeColor(): React.JSX.Element {
     return (
         <div>
             <h3>Change Color</h3>
-            <Form.Check
-                inline
-                type = "radio"
-                name = "color"
-                onChange = {updateColor}
-                id = "color-check-red"
-                label = "Red"
-                value = "red"
-                checked = {color === "red"}
-            />
-            <Form.Check
-                inline
-                type = "radio"
-                name = "color"
-                onChange = {updateColor}
-                id = "color-check-orange"
-                label = "Orange"
-                value = "orange"
-                checked = {color === "orange"}
-            />
+            {colors.map((c) => (
+                <Form.Check
+                    inline
+                    key = {c}
+                    type = "radio"
+                    name = "color"
+                    onChange = {updateColor}
+                    id = {`color-check-${c}`}
+                    label = {c}
+                    value = {c}
+                    checked = {color === c}
+                />
+            ))}
             <div>You have chosen <span style={{backgroundColor: color}}>{color}</span>.</div>
         </div>
     );
